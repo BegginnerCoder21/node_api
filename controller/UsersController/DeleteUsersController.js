459442ea5dd7f7394d81a70c9c53bb6245a9d07b
Teguera <aboubacarteguera21@example.com>
2023-11-29 15:01:54 +0000

    suppression d'un utilisateur implementé et fin du projet

--- controller/UsersController/DeleteUsersController.js
+++ controller/UsersController/DeleteUsersController.js
@@ -3,4 +3,20 @@ const mongoose = require('mongoose');
 const urldb = "mongodb://127.0.0.1:27017/usersdb";
 const user_model = require('../../models/UsersModel');
 
-const deleteUsers = async (req,res)
+const deleteUsers = async (req,res) => {
+    try {
+        let idUser = new mongoose.Types.ObjectId(req.params.userId);
+        connect(urldb);
+        const user = await user_model.deleteOne({_id : idUser});
+        if(user.deletedCount === 1){
+            res.status(200).json({messageSuccess : "L'utilisateur a été supprimer avec succès"});
+        }else{
+            res.status(404).json({messageError404 : "Aucun utilisateur n'a été trouvé"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({messageError500 : "Connexion à la base donnée échoué"});
+    }
+}
+
+module.exports = deleteUsers;
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routerUser = express.Router();
 const addUsers = require('../controller/UsersController/AddUsersController');
+const deleteUsers = require('../controller/UsersController/DeleteUsersController');                                
 const getAllUsers = require('../controller/UsersController/GetAllUsersController');
 const updatedUsers = require('../controller/UsersController/UpdatedUsersController');
 
 routerUser.route('/addUsers').post(addUsers);
 routerUser.route('/allUsers').get(getAllUsers);
 routerUser.route('/updatedUsers/:userId').put(updatedUsers);
+routerUser.route('/deleteUsers/:userId').delete(deleteUsers);
 
 module.exports = routerUser;
